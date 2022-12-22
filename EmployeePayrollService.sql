67d596c59791064b525de0d162c688db554f8bce
AnilBamne <anilbamne20@gmail.com>
2022-12-22 18:45:56 +0530

    Adding:UC4RetrivingAllDataFromTable code

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -13,3 +13,5 @@ StartDate date
 --- uc3 CURD operations, INSERT INTO ---
 Insert Into EmployeePayroll values('Virat',65400,'2020-08-12');   --- ID is auto increamented
 Insert Into EmployeePayroll values('Dhoni',89546,'1998-02-22'),('Sachin',79546,'1995-01-01'),('Rahul',82544,'2022-10-02')
+--- uc4 Retriving all data ---
+Select * from EmployeePayroll
