05521ccf339a2b006a54cda584d673ed102a1ff3
AnilBamne <anilbamne20@gmail.com>
2022-12-22 18:30:09 +0530

    Adding:UC2CreateTableEmployeePayroll code

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -2,3 +2,9 @@
 Create database PayrollService
 --- Going to data base ---
 use PayrollService
+create table EmployeePayroll (
+ID int identity(1,1) primary key,
+Name varchar(200),
+Salary float,
+StartDate date
+);
