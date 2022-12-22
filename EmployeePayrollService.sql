800f6bab0be9fb677630bc54f474b76ec64af16b
AnilBamne <anilbamne20@gmail.com>
2022-12-22 19:17:20 +0530

    Adding:UC7AbilityToFindSumAvgMaxMin code

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -25,3 +25,16 @@ Alter Table EmployeePayroll add Gender char(1);
 Update EmployeePayroll set Gender='M';
 Insert Into EmployeePayroll values('Kalpana',29546,'2002-02-09','F'),('Snehal',32541,'2022-01-30','F'),('Kirthi',22587,'1999-11-01','F')
 Select * from EmployeePayroll
+--- uc7 sum, average, min, max quaries ---
+select sum(Salary) from EmployeePayRoll 
+select sum(Salary) as Totalsalary from EmployeePayRoll 
+select sum(Salary) from EmployeePayRoll group by Gender;
+select sum(Salary) as Totalsalary,Gender from EmployeePayRoll group by Gender;
+select max(Salary) as Totalsalary,Gender from EmployeePayRoll group by Gender;
+select * from EmployeePayRoll where Salary=(select Max(salary) as totalsalary from employeepayroll where gender='F' )
+select * from EmployeePayRoll where Salary=(select Max(salary) as totalsalary from employeepayroll where gender='M' )
+select * from EmployeePayRoll where Salary in (select Max(salary) as totalsalary from employeepayroll group by gender)
+select name,Salary,Gender from EmployeePayRoll where salary in (select Max(salary) as totalsalary from employeepayroll group by gender)
+select name,Salary,Gender from EmployeePayRoll where salary in (select Min(salary) as totalsalary from employeepayroll group by gender)
+select avg(salary) as Totalsalary, gender from EmployeePayRoll group by gender
+select avg(salary) as Totalsalary from EmployeePayRoll;
